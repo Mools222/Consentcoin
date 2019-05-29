48af761d95c4a81f937389855a8723299895d4f2
DanORLarsen <42993978+DanORLarsen@users.noreply.github.com>
2019-05-29 18:34:53 +0100

    Adding JavaDoc to DAOInterface

--- app/src/main/java/com/thomosim/consentcoin/Persistence/DAOInterface.java
+++ app/src/main/java/com/thomosim/consentcoin/Persistence/DAOInterface.java
@@ -14,41 +14,95 @@ import com.thomosim.consentcoin.Persistence.ModelClass.User;
 
 import java.util.ArrayList;
 import java.util.Date;
-
+/**
+ * The DAOinterface is made to make it easy to change from one Database to another,
+ * and to ensure all databases have same methods
+ */
 public interface DAOInterface {
 
+    /**
+     * This method creates an AuthStateListener on the database
+     */
     void addAuthStateListener();
 
+    /**
+     * This method removes an AuthStateListener from the database
+     */
     void removeAuthStateListener();
 
+    /**
+     * This method sets the Reference to the CurrentUser of the database
+     */
     void setDatabaseReferenceCurrentUser();
 
+    /**
+     * This method adds a Database listener to the User Reference
+     */
     void addDatabaseListenerUser();
 
+    /**
+     * This method creates a listener on the database
+     */
     void addDatabaseListener();
 
+    /**
+     * This method removes a listener on the database
+     */
     void removeDatabaseListener();
 
+    /**
+     * This returns the signIn Intent
+     */
     Intent getSignInIntent();
 
+    /**
+     * This method handles signOut from the database
+     */
     void signOut(Context context);
 
+    /**
+     * This method adds an Authentication to database
+     */
     void addAuthentication();
 
+    /**
+     * This method returns a authenticationObject (Generic)
+     */
     <T extends MyObservable> T getAuthentication(); // Firebase authentication comes in a FirebaseAuth object. When switching DAO, we'd probably use a different object. Therefore the generic type T and raw type MyObservable is used to accommodate all possibilities.
 
+    /**
+     * This method updates the authenticationObject in the database (Generic)
+     */
     <T> void updateAuthentication(String id, T t);
 
+    /**
+     * This method removes an authenticationObject from the database
+     */
     <T> void removeAuthentication(T t);
 
+    /**
+     * This method adds a User to the database
+     */
     void addUser(String userType, String uid, String userEmail, String userDisplayName, String organizationName);
 
+    /**
+     * This method returns a User
+     */
     MyObservable<User> getUser();
 
+    /**
+     * This method returns an Arraylist of User objects
+     */
     MyObservable<ArrayList<User>> getUsers();
 
+    /**
+     * This method update an existing user
+     */
     void updateUser(String uid, User user);
 
+    /**
+     * This method removes a User
+     */
     void removeUser(User user);
 
     void addPermissionRequest(String organizationName, String organizationUid, String memberName, String memberUid, ContractTypeEnum permissionType, Date creationDate, Date permissionStartDate, Date permissionEndDate, ContractScopeEnum personsIncluded);
