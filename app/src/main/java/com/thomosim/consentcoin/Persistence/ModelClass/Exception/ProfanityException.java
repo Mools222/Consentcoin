d0f2de37c7c9ed075583aceb81000c0ba5847dc9
DanORLarsen <42993978+DanORLarsen@users.noreply.github.com>
2019-05-30 19:35:31 +0100

    Exception startet

--- app/src/main/java/com/thomosim/consentcoin/Persistence/ModelClass/Exception/ProfanityException.java
+++ app/src/main/java/com/thomosim/consentcoin/Persistence/ModelClass/Exception/ProfanityException.java
@@ -1,16 +1,12 @@
 package com.thomosim.consentcoin.Persistence.ModelClass.Exception;
 
 public class ProfanityException extends Exception {
-    String sentence;
 
-    public ProfanityException(String sentence){
-        this.sentence = sentence;
-        if (sentence.toLowerCase().contains("fuck")){
-            System.out.println("Profanity in the sentence \"" +sentence+"\"");
+    public ProfanityException( ){
+        super();
         }
-    }
 
     public String toString(){
-        return "Profanity in the sentence \"" +sentence+"\"";
+        return "Profanity was found in the word";
     }
 }
